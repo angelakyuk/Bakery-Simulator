9c25fa54d16fcd3e13ca1533533d8d048b045340
Ethan02317 <elg2023@gmail.com>
2026-04-14 14:07:46 -0400

    updated 'simulator-main.py'

--- simulator_main.py
+++ simulator_main.py
@@ -72,3 +72,33 @@ def handle_unlocks(money, recipes):
             info["unlocked"] = False
 
     return recipes
+
+#Ethan Gustave's Function
+from random import choice
+
+def create_customers(self, num, customers):
+    """Creates a list of customers from the amount specified for the day
+
+    Args: 
+	    num(int): the number of customers to be generated based on ad level.
+	    customers(dict): the customers to be chosen from. Key is customer name,
+        associated value is a customer object.
+
+    Returns: 
+        A list of dictionaries. In the dictionary, the key will be the customer 
+        name and the associated value will be a reference to a customer object.
+    """
+    
+    if num > len(customers):
+        num = len(customers)
+        
+    customer_start = customers.copy()
+    customer_final = []
+    count = 0
+    
+    while(count < num):
+        curr = choice(customer_start.keys())
+        customer_final.append(curr, customer_start.pop(curr))
+        count += 1
+        
+    return customer_final
