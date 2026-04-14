c57d129955587811f8bc256ed579fefca49299a7
Sarayu Vanam <Sarayu@sarayu-macbook.wireless.dyn.umd.edu>
2026-04-13 22:10:33 -0400

    Adding handle_unlocks into simulator_main.py

--- simulator_main.py
+++ simulator_main.py
@@ -46,3 +46,29 @@ def prompt_request():
         prompt_request()
     elif more.lower() == 'n':
         fulfill_request('continue')
+        
+#Sarayu Vanam's function
+def handle_unlocks(money, recipes):
+    """
+    Determines which recipe is unlocked based on how much money the baker has.
+
+    Args:
+        money (float): The amount of money the player currently has.
+        recipes (dict): A dictionary of recipes where each recipe includes 
+                        its name, price, and locked/unlocked status.
+
+    Side Effects:
+        Modifies the recipes dictionary by updating which recipes are 
+        locked or unlocked for the player.
+
+    Returns:
+        dict: The updated dictionary of recipes with their current unlock status
+    """
+
+    for name, info in recipes.items():
+        if money >= info["price"]:
+            info["unlocked"] = True
+        else:
+            info["unlocked"] = False
+
+    return recipes
